71d3a93bb2462a54f4dbbf38113a4890934d1a63
vikas reddy <48214808+vikasreddykunapally@users.noreply.github.com>
2019-03-11 21:13:28 +0530

    Update splitbill.py

--- splitbill.py
+++ splitbill.py
@@ -1,8 +1,8 @@
 from flask import Flask,render_template
 app = Flask(__name__)
 @app.route('/')
-   def list(): 
-return render_template("split.html",show=splitbill)
+def list(): 
+      return render_template("split.html")
 
 if __name__ == "__main__":   
 app.run()
